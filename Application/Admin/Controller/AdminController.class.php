7d0c6e275e42604f2a2e5f7a09baec5dd83b5f08
LiTao <1287862489@qq.com>
2016-08-26 09:25:51 +0800

    TP

--- Application/Admin/Controller/AdminController.class.php
+++ Application/Admin/Controller/AdminController.class.php
@@ -1,8 +1,7 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
-class AdminController extends CommonController {
-    
+class AdminController extends CommonController { 
     /**
      * 读取管理员
      */
@@ -16,8 +15,31 @@ class AdminController extends CommonController {
      * 添加管理员
      */
     public function addadmin(){
-        
-    	$this->display();
+        // 判断提交方式 做不同处理
+        if (IS_POST) {
+            // 实例化admin对象
+            $admin = D('admin');
+            $data['username']= $_POST['username'];
+            $data['password']= md5($_POST['password']);
+            $data['email']= $_POST['email'];
+            $data['regtime']= time();
+            
+            // 自动验证 创建数据集
+            if (!$data = $admin->create()) {
+                // 防止输出中文乱码
+                header("Content-type: text/html; charset=utf-8");
+                exit($admin->getError());
+            }
+
+            //插入数据库
+            if ($id = M('admin')->add($data)) {
+                $this->success('添加成功', U('Admin/index'), 2);
+            } else {
+                $this->error('添加失败');
+            }
+        } else {
+            $this->display();
+        }
     }
 
 }
